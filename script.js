619c6a68eb73f607df4cc9f4b5dbebe7c18fae71
CraigYeoman <yeomancraig@gmail.com>
2022-01-29 11:58:12 -0600

    Added color chang and background color change

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
+// Variables
+const LOADING_GRID_COLOR = "#FFFFF";
+const LOADING_COLOR = "black";
+const LOADING_SIZE = 10;
+
+let gridColor = LOADING_GRID_COLOR;
+let gridSquareColor = LOADING_COLOR;
+let gridSize = LOADING_SIZE;
+
 // Get ElementById
 const grid = document.getElementById('grid');
 const gridSizeButton = document.getElementById('grid-size');
 const resetButton = document.getElementById('reset-button');
+const gridColorButton = document.getElementById('grid-color');
+const backgroundColorButton = document.getElementById('grid-background-color');
 
 // EventListners
-gridSizeButton.addEventListener('click', gridCreation);
+gridSizeButton.addEventListener('click', gridResize);
 resetButton.addEventListener('click', gridColorReset);
+gridColorButton.addEventListener('input', (e) => gridSquareColor = e.target.value );
+backgroundColorButton.addEventListener('input', backgroundColorChange);
 
 //Functions
 function colorChange(e) {  
-    this.style.backgroundColor = "black";
+    this.style.backgroundColor = gridSquareColor;
+    this.id = 'color-changed';
     e.stopPropagation();
 }
 
-function gridCreation () {
+function gridCreation (size) {
     newGrid();
-    let userInput = prompt("Please Enter Size of Grid less than 100");
-    for (let i = 0; i < userInput; i++) {
+    for (let i = 0; i < size; i++) {
         let row = document.createElement('div');
         row.id = 'row';
         row.className = 'row';
         grid.appendChild(row);
-        for (let i = 0; i < userInput; i++) {
+        for (let i = 0; i < size; i++) {
             const div = document.createElement('div');
             div.id = 'grid-squares';
             div.className = 'grid-squares';
@@ -40,5 +53,20 @@ function newGrid() {
 
 function gridColorReset() {
     let gridSquares = document.querySelectorAll('div.grid-squares');
-    gridSquares.forEach(div => div.style.backgroundColor = "black");
+    gridSquares.forEach(div => div.style.backgroundColor = "rgb(255, 255, 255)");
+}
+
+function gridResize () {
+    let size = prompt("Please Enter Size of Grid less than 100");
+    gridCreation(size);
+}
+
+function backgroundColorChange(e) {
+    gridColor = e.target.value;
+    let gridSquares = document.querySelectorAll('div#grid-squares');
+    gridSquares.forEach(div => div.style.backgroundColor = gridColor);
+}
+
+window.onload = () => {
+    gridCreation(LOADING_SIZE);
 }
